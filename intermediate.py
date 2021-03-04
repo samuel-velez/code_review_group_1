7b731e3958b3fb050dd9a12348ecc62f4a1fff2e
Aamir <jadoon.engr@gmail.com>
2021-03-04 23:02:27 +0400

    Update intermediate.py

--- intermediate.py
+++ intermediate.py
@@ -1,16 +1,15 @@
-# Intermediate function(s)
-## Code Attribution: Code snippets taken and adapted from https://towardsdatascience.com/how-to-be-pythonic-and-why-you-should-care-188d63a5037e
-
-
-def find_string_in_list(string_looking = "cherries", 
-						list_finding = ["apples", "oranges", "bananas", "grapes"] ):
+def find_string_in_list(string_looking = "cherries",
+list_finding = ["apples", "oranges", "bananas", "grapes"] ):
     """ 
     This function allegedly looks for string_looking in list_finding and returns the string_looking if the string is found in the link. But what does it do if it can't find the string? WHO KNOWS....
     """
-	found = False
-	size = len(arr)
-	for i in range(0, size):
-	    if arr[i] == s:
-	        found = True
+    
+    found = False
+    size = len(list_finding)
+    for i in range(0, size):
+        if list_finding[i] == string_looking:
+            found = True
 
-	return string_looking
+    return found
+
+find_string_in_list()
